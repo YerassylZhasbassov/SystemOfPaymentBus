5ebf6cd26240ffdd86912ffa61256b5f3888067f
YerassylZhasbassov <yera72349@gmail.com>
2020-05-25 21:11:31 +0600

    class Payment is done

---
+++ examLesson/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace examLesson
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
